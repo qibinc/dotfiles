6cf33f73c09a588b26bc7655b99dfecee640f116
Qibin Chen <chenqibin422@gmail.com>
2022-01-25 00:05:11 -0500

    Fix brew

--- brew.sh
+++ brew.sh
@@ -2,6 +2,9 @@
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$($HOME/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+
 brew update
 brew upgrade
 
