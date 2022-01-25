52d66971fab8830e775acb4c10201884bb4cafdb
Qibin Chen <chenqibin422@gmail.com>
2022-01-25 01:15:18 -0500

    Fix

--- brew.sh
+++ brew.sh
@@ -8,7 +8,9 @@ eval "$($HOME/.linuxbrew/bin/brew shellenv)"
 brew update
 brew upgrade
 
+brew install gcc
 brew install zsh
+brew install htop
 brew install bat
 brew install dust
 brew install duf
