d1ab7fbabfe6712b0ecda62e51fdaed05fe6aa2b
Qibin Chen <chenqibin422@gmail.com>
2022-01-25 21:37:30 -0500

    Fix

--- brew.sh
+++ brew.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo 'eval "$($HOME/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
-eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+if ! command -v brew; then
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$($HOME/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+  eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+fi
 
 brew update
 brew upgrade
 
-if ! command -v zs; then
+if ! command -v zsh; then
   brew install zsh
 fi
 if ! command -v htop; then
