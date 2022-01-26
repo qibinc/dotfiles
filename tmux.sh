706702736ef79115ebbc5d60431b69db53d25cf6
Qibin Chen <chenqibin422@gmail.com>
2022-01-25 21:38:39 -0500

    Fix

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd
+rm -rf .tmux/
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
